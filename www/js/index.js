94730a7cd3c66a0143ffc3f935ce4a198d2c3b9a
ignaskara <55839284+ignaskara@users.noreply.github.com>
2019-10-17 14:53:23 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -52,8 +52,8 @@ function updateDisplay() {
 		
 		//document.getElementById("myText").innerHTML = value;
 		$("#myText").text(value);
-		$("#lenght").text(value);
-		$("#myMarsBar").text(value);
+		$("#lenght").text(lenght);
+		$("#myMarsBar").text(getMars);
 		
 		
 		
@@ -121,4 +121,85 @@ function updateDisplay() {
 	updateDisplay();
 	    
 	alert("resume");
+	alert("device ready");
+	var key = "pen";
+		var value = "blue colour";
+				
+		window.localStorage.setItem(key, value);
+		
+		var key = "headset";
+		var value = "Green";
+				
+		window.localStorage.setItem(key, value);
+		
+		var key = "mouse";
+		var value = "small";
+				
+		window.localStorage.setItem(key, value);
+		
+		var key = "window";
+		var value = "clear";
+				
+		window.localStorage.setItem(key, value);
+		
+		var key = "carpet";
+		var value = "old";
+				
+		window.localStorage.setItem(key, value);
+		
+		var value = window.localStorage.getItem(key);
+		
+		//document.getElementById("myText").innerHTML = value;
+		$("#myText").text(value);
+		$("#lenght").text(lenght);
+		$("#myMarsBar").text(getMars);
+		
+		
+		
+		
+		/**  part for JSON  **/
+		
+		
+		
+		// STORING nutritional information of a mars choco bar.
+		
+		var marsBarInfo= {
+			"Calories" : "230g",
+			"Total fat" {
+				"Total fat" : "8.6g",
+				"Saturated fat" : "4.2g",
+				"Trans fat" : "0g"
+			}
+			"Cholesterol" : "0mg",
+			"Sodium": "0mg",
+			"Potassium" : "0mg",
+			"Total Carbohydrate" {
+				"Total Carbohydrate" : "35.3g",
+				"Dietary Fiber" : "0g",
+				"Sugars" "30.5g"
+			}
+			"Protein": "2.2g",
+			"Vitamins" {
+				"Vitamin A" : "0%",
+				"Calcium" : "0%",
+				"Vitamin C" : "0%",
+				"Iron" : "0%"
+			}
+			
+		}
+		
+		//MAKE mars into a string. haha
+		var chocoBarAsAString = JSON.stringify(marsBarInfo);
+		
+		//STORE it into local storage
+		window.localStorage.setItem("adress", chocoBarAsAString);
+		
+		//ACCESS mars as a string
+		var getMars = window.localStorage.getItem("adress");
+		
+		//UNSTRING mars bar
+		var barJSON =JSON.parse(getMars);
+		
+		
+		/** The end of JSON **/
     }
